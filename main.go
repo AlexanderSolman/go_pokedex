ab23295f53494fef0b02e8bd455a10462c3f764d
Alex Andersson <alex.andersson@bruhnspace.com>
2024-02-12 20:51:36 +0100

    Fixed so map and mapb works as intended, with cache

--- main.go
+++ main.go
@@ -46,10 +46,14 @@ func commands() map[string]cliCommand {
 			name:        "mapb",
 			description: "Display previous 20 location areas",
 		},
+		"explore": {
+			name:        "explore",
+			description: "List pokémon in given area",
+		},
 	}
 }
 
-func parseJsonResponse(res *http.Response, locationArea *jsonResponse) {
+func parseJsonResponse(res *http.Response, locationArea *jsonResponse) []byte {
 	body, err := io.ReadAll(res.Body)
 	res.Body.Close()
 
@@ -63,17 +67,21 @@ func parseJsonResponse(res *http.Response, locationArea *jsonResponse) {
 	errr := json.Unmarshal(body, &locationArea)
 	if errr != nil {
 		fmt.Println(errr, "\nCould not parse json")
-		return
+		return nil
 	}
 
+	addLocations := []byte{}
 	for _, i := range locationArea.Results {
 		fmt.Println(i.Name)
+		addLocations = append(addLocations, []byte(i.Name+"\n")...)
 	}
+	return addLocations
 }
 
 func main() {
 	var locationArea jsonResponse
-	pokecache.NewCache(5 * time.Minute) //test line //TO BE MOVED
+	cache := pokecache.NewCache(5 * time.Minute) // Cache created at start with 5min interval
+
 	for {
 		fmt.Println("pokedex >")
 
@@ -94,35 +102,60 @@ func main() {
 			fmt.Println("map:  ", m_com["map"].description)
 			fmt.Println("mapb: ", m_com["mapb"].description)
 			fmt.Println("exit: ", m_com["exit"].description, "\n")
-		case "exit":
-			return
 		case "map":
+			// Initial call to the API given no call has been made.
+			// Data gets printed and added to the cache
 			if locationArea.Next == "" {
 				res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
 				if err != nil {
 					log.Fatal(err)
 				}
-				parseJsonResponse(res, &locationArea)
+				data := parseJsonResponse(res, &locationArea)
+				cache.Add("https://pokeapi.co/api/v2/location-area/?offset=0&limit=20", data, locationArea.Next, locationArea.Previous)
 			} else {
-				res, err := http.Get(locationArea.Next)
-				if err != nil {
-					log.Fatal(err)
+				// Checks if data is cached and prints else calls API for it and adds to cache
+				if i, ok, n, p := cache.Get(locationArea.Next); ok {
+					fmt.Println("From cache: \n")
+					fmt.Println(string(i))
+					locationArea.Next = n
+					locationArea.Previous = p
+				} else {
+					url := locationArea.Next
+					res, err := http.Get(locationArea.Next)
+					if err != nil {
+						log.Fatal(err)
+					}
+					data := parseJsonResponse(res, &locationArea)
+					cache.Add(url, data, locationArea.Next, locationArea.Previous)
 				}
-				parseJsonResponse(res, &locationArea)
 			}
 		case "mapb":
 			if locationArea.Previous == "" {
 				fmt.Println("There were no previous locations")
 			} else {
-				res, err := http.Get(locationArea.Previous)
-				if err != nil {
-					log.Fatal(err)
+				// Checks if data is cached and prints else calls API for it and adds to cache
+				if i, ok, n, p := cache.Get(locationArea.Previous); ok {
+					fmt.Println("From cache: \n")
+					fmt.Println(string(i))
+					locationArea.Next = n
+					locationArea.Previous = p
+				} else {
+					res, err := http.Get(locationArea.Previous)
+					if err != nil {
+						log.Fatal(err)
+					}
+					data := parseJsonResponse(res, &locationArea)
+					cache.Add(locationArea.Previous, data, locationArea.Next, locationArea.Previous)
 				}
-				parseJsonResponse(res, &locationArea)
 			}
+		case "explore":
+
+		case "exit":
+			return
 		default:
 			fmt.Println("Type <help> for information on usage")
 		}
+		// TEMP prints to check next and prev pointers
 		fmt.Println("Url for next: ", locationArea.Next)
 		fmt.Println("Url for previous: ", locationArea.Previous)
 	}
